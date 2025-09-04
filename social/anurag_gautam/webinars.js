b562c1aa4b126433552fbc55088a7b70801edf23
atikle <atikle.com@gmail.com>
2025-09-04 15:02:08 +0530

    Update

---
+++ social/anurag_gautam/webinars.js
@@ -0,0 +1,19 @@
+const webinars = [
+    {
+        title: 'Introduction to Algorithmic Trading',
+        hostUsername: 'anurag_gautam',
+        date: 'August 15, 2025',
+        thumbnailUrl: 'https://images.unsplash.com/photo-1556740758-90de374c12ad?q=80&w=2070',
+        recordingUrl: '#',
+        filesUrl: '#'
+    },
+    {
+        title: 'Understanding Blockchain & Crypto Assets',
+        hostUsername: 'anurag_gautam',
+        date: 'July 28, 2025',
+        thumbnailUrl: 'https://images.unsplash.com/photo-1639322537228-f710d846310a?q=80&w=1932',
+        recordingUrl: '#',
+        filesUrl: '#'
+    }
+    // Add new webinars here by copying the object above
+];
