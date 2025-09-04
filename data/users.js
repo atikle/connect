fc08b0b966fa344d8388ae19e0d008d4648b4744
atikle <atikle.com@gmail.com>
2025-09-04 15:13:36 +0530

    Update

--- data/users.js
+++ data/users.js
@@ -2,7 +2,7 @@ const users = {
     'anurag_gautam': {
         name: 'Anurag Gautam',
         username: '@anurag_gautam',
-        ConnectProfileLink: 'https://atikle.github.io/connect/social/anurag_gautam',
+        ConnectProfileUrl: 'https://atikle.github.io/connect/social/anurag_gautam',
         avatarUrl: 'https://atikle.github.io/connect/data/application/avatars/anurag_gautam.jpg',
         bio: 'Passionate web developer with a love for clean code and intuitive design. I enjoy building applications that solve real-world problems and creating engaging user experiences.',
         stats: {
@@ -21,7 +21,7 @@ const users = {
     'aditya': {
         name: 'Aditya',
         username: '@aditya',
-        ConnectProfileLink: 'https://atikle.github.io/connect/social/aditya',
+        ConnectProfileUrl: 'https://atikle.github.io/connect/social/aditya',
         avatarUrl: 'https://atikle.github.io/connect/data/application/avatars/aditya.jpg',
         bio: 'UX Designer and front-end enthusiast.',
         stats: {
@@ -40,7 +40,7 @@ const users = {
     'ananya': {
         name: 'Ananya',
         username: '@ananya',
-        ConnectProfileLink: 'https://atikle.github.io/connect/social/ananya',
+        ConnectProfileUrl: 'https://atikle.github.io/connect/social/ananya',
         avatarUrl: 'https://atikle.github.io/connect/data/application/avatars/ananya.jpg',
         bio: 'UX Designer and back-end enthusiast.',
         stats: {
