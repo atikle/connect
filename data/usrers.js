c801dcf3b9c95f1f1b6cb7f3057166ecc184d6a7
atikle <atikle.com@gmail.com>
2025-09-04 13:58:47 +0530

    Update

---
+++ data/usrers.js
@@ -0,0 +1,34 @@
+const users = {
+    'anurag_gautam': {
+        name: 'Anurag Gautam',
+        username: '@anurag_gautam',
+        avatarUrl: 'https://atikle.github.io/connect/data/application/avatar/anurag_gautam.jpg',
+        bio: 'Passionate web developer...',
+        stats: {
+            posts: 2,
+            webinars: 7
+        },
+        socials: {
+            instagram: '#',
+            facebook: '#',
+            twitter: '#',
+            linkedin: '#',
+            github: '#'
+        }
+    }, // <-- Make sure this comma is here before adding the next user
+
+    'aditya': {
+        name: 'Aditya',
+        username: '@aditya',
+        avatarUrl: 'https://via.placeholder.com/150', // Placeholder image
+        bio: 'UX Designer and front-end enthusiast.',
+        stats: {
+            posts: 10,
+            webinars: 3
+        },
+        socials: {
+            twitter: '#',
+            linkedin: '#'
+        }
+    } // The very last entry doesn't need a comma after it
+};
