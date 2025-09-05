97c91fa42eae84defce9ca9ae5636d6ef9c7e131
atikle <atikle.com@gmail.com>
2025-09-05 16:04:14 +0530

    Update

---
+++ data/admin.js
@@ -0,0 +1,11 @@
+const users = {
+    'atikle_connect': {
+        name: 'atikle connect',
+        username: '@atikle_connect',
+        avatarUrl: 'https://atikle.github.io/connect/data/application/resources/admin.png',
+        stats: {
+            posts: 2,
+            webinars: 7
+        },
+    },
+};
