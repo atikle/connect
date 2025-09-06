3dc9f1de71ab377770baec6b42132766e03381c3
atikle <atikle.com@gmail.com>
2025-09-06 14:14:27 +0530

    Update

---
+++ schedule.js
@@ -0,0 +1,58 @@
+const webinars = {
+    'webinar-1': {
+        name: 'Pilot Testing Session 1',
+        username: '@anurag_gautam',
+        ConnectProfileUrl: 'https://atikle.github.io/connect/community/anurag_gautam',
+        avatarUrl: 'https://atikle.github.io/connect/data/application/avatars/anurag_gautam.jpg',
+        bio: 'Passionate web developer with a love for clean code and intuitive design. I enjoy building applications that solve real-world problems and creating engaging user experiences.',
+        stats: {
+            posts: 2,
+            webinars: 7
+        },
+        socials: {
+            instagram: '#',
+            facebook: '#',
+            twitter: '#',
+            linkedin: '#',
+            github: '#'
+        }
+    },
+
+    'ananya': {
+        name: 'Ananya',
+        username: '@ananya',
+        ConnectProfileUrl: 'https://atikle.github.io/connect/community/ananya',
+        avatarUrl: 'https://atikle.github.io/connect/data/application/avatars/ananya.jpg',
+        bio: 'UX Designer and back-end enthusiast.',
+        stats: {
+            posts: 10,
+            webinars: 3
+        },
+        socials: {
+            instagram: '#',
+            facebook: '#',
+            twitter: '#',
+            linkedin: '#',
+            github: '#'
+        }
+    },
+
+    'aditya': {
+        name: 'Aditya',
+        username: '@aditya',
+        ConnectProfileUrl: 'https://atikle.github.io/connect/community/aditya',
+        avatarUrl: 'https://atikle.github.io/connect/data/application/avatars/aditya.jpg',
+        bio: 'UX Designer and front-end enthusiast.',
+        stats: {
+            posts: 10,
+            webinars: 3
+        },
+        socials: {
+            instagram: '#',
+            facebook: '#',
+            twitter: '#',
+            linkedin: '#',
+            github: '#'
+        }
+    },
+};
