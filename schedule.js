27bfdd0e99c94354bcb9f1cdff5c9590dce4b274
atikle <atikle.com@gmail.com>
2025-09-06 15:20:17 +0530

    Update

--- schedule.js
+++ schedule.js
@@ -1,39 +1,35 @@
-const webinars = {
-    'webinar-1': {
-        name: 'Pilot Testing Session 1',
-        date: '20th June 2025',
+const future_webinars = {
+    'webinar-8': {
+        name: 'Pilot Testing Session 8',
+        date: 'Always 15th August 2025',
         StartTime: '5PM',
-        duration: '63 minutes',
+        duration: '',
         recordingUrl: 'https://atikle.github.io/connect/recordings',
         filesUrl: 'https://atikle.github.io/connect/files',
         thumbnailUrl: 'https://atikle.github.io/resource/atikle_connect/video-thumbnail.svg'
-    },
+    }
+};
 
-    'webinar-2': {
-        name: 'Pilot Testing Session 2',
-        date: '25th June 2025',
-        StartTime: '5PM',
-        duration: '42 minutes',
+const present_webinars = {
+    'webinar-7': {
+        name: 'Pilot Testing Session 7 (active window)',
+        date: 'Always active',
+        StartTime: '',
+        duration: '',
         recordingUrl: 'https://atikle.github.io/connect/recordings',
         filesUrl: 'https://atikle.github.io/connect/files',
         thumbnailUrl: 'https://atikle.github.io/resource/atikle_connect/video-thumbnail.svg'
-    },
+    }
+};
 
-    'webinar-3': {
-        name: 'Pilot Testing Session 3',
-        date: '30th June 2025',
-        StartTime: '5PM',
-        duration: '30 minutes',
-        recordingUrl: 'https://atikle.github.io/connect/recordings',
-        filesUrl: 'https://atikle.github.io/connect/files',
-        thumbnailUrl: 'https://atikle.github.io/resource/atikle_connect/video-thumbnail.svg'
-    },
 
-    'webinar-4': {
-        name: 'Pilot Testing Session 4',
-        date: '2nd July 2025',
+
+const past_webinars = {
+    'webinar-6': {
+        name: 'Pilot Testing Session 6',
+        date: '11th July 2025',
         StartTime: '5PM',
-        duration: '126 minutes',
+        duration: '39 minutes',
         recordingUrl: 'https://atikle.github.io/connect/recordings',
         filesUrl: 'https://atikle.github.io/connect/files',
         thumbnailUrl: 'https://atikle.github.io/resource/atikle_connect/video-thumbnail.svg'
@@ -49,13 +45,43 @@ const webinars = {
         thumbnailUrl: 'https://atikle.github.io/resource/atikle_connect/video-thumbnail.svg'
     },
 
-    'webinar-6': {
-        name: 'Pilot Testing Session 6',
-        date: '11th July 2025',
+    'webinar-4': {
+        name: 'Pilot Testing Session 4',
+        date: '2nd July 2025',
         StartTime: '5PM',
-        duration: '39 minutes',
+        duration: '126 minutes',
         recordingUrl: 'https://atikle.github.io/connect/recordings',
         filesUrl: 'https://atikle.github.io/connect/files',
         thumbnailUrl: 'https://atikle.github.io/resource/atikle_connect/video-thumbnail.svg'
     },
+
+    'webinar-3': {
+        name: 'Pilot Testing Session 3',
+        date: '30th June 2025',
+        StartTime: '5PM',
+        duration: '30 minutes',
+        recordingUrl: 'https://atikle.github.io/connect/recordings',
+        filesUrl: 'https://atikle.github.io/connect/files',
+        thumbnailUrl: 'https://atikle.github.io/resource/atikle_connect/video-thumbnail.svg'
+    },
+
+    'webinar-2': {
+        name: 'Pilot Testing Session 2',
+        date: '25th June 2025',
+        StartTime: '5PM',
+        duration: '42 minutes',
+        recordingUrl: 'https://atikle.github.io/connect/recordings',
+        filesUrl: 'https://atikle.github.io/connect/files',
+        thumbnailUrl: 'https://atikle.github.io/resource/atikle_connect/video-thumbnail.svg'
+    },
+
+    'webinar-1': {
+        name: 'Pilot Testing Session 1',
+        date: '20th June 2025',
+        StartTime: '5PM',
+        duration: '63 minutes',
+        recordingUrl: 'https://atikle.github.io/connect/recordings',
+        filesUrl: 'https://atikle.github.io/connect/files',
+        thumbnailUrl: 'https://atikle.github.io/resource/atikle_connect/video-thumbnail.svg'
+    }
 };
