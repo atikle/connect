d2cc656f5b6b9ab88b88f9c5ee8a02d32e4880cf
atikle <atikle.com@gmail.com>
2025-10-02 13:55:15 +0530

    Update

--- community/anurag_gautam/posts.js
+++ community/anurag_gautam/posts.js
@@ -3,18 +3,24 @@ const posts = [
         authorUsername: 'anurag_gautam',
         timestamp: 'October 1, 2025',
         text: 'Excited to share the new atikle account login page!. What do you think?',
-        imageUrl: 'https://atikle.github.io/connect/community/anurag_gautam/posts/img-4.jpg'
+        imageUrl: 'https://atikle.github.io/connect/community/anurag_gautam/posts/img-5.jpg'
     },
     {
         authorUsername: 'anurag_gautam',
         timestamp: 'September 15, 2025',
         text: 'Excited to share the revamped profile page for atikle community! Added smooth animations and new sections for posts and webinars. What do you think?',
-        imageUrl: 'https://atikle.github.io/connect/community/anurag_gautam/posts/img-3.jpg'
+        imageUrl: 'https://atikle.github.io/connect/community/anurag_gautam/posts/img-4.jpg'
     },
     {
         authorUsername: 'anurag_gautam',
         timestamp: 'September 1, 2025',
         text: 'Excited to share the revamped profile page for atikle community! What do you think?',
+        imageUrl: 'https://atikle.github.io/connect/community/anurag_gautam/posts/img-3.jpg'
+    },
+    {
+        authorUsername: 'anurag_gautam',
+        timestamp: 'August 1, 2025',
+        text: 'Excited to announce atikle connect webinar application!',
         imageUrl: 'https://atikle.github.io/connect/community/anurag_gautam/posts/img-2.jpg'
     },
     {
