2da556db971a2ef2099b3c1cfe698715345daf29
atikle <atikle.com@gmail.com>
2025-10-02 16:37:24 +0530

    Update

--- community/anurag_gautam/posts.js
+++ community/anurag_gautam/posts.js
@@ -25,15 +25,14 @@ const posts = [
     },
     {
         authorUsername: 'anurag_gautam',
-        timestamp: 'August 1, 2025',
-        text: 'Excited to announce atikle connect!',
+        timestamp: 'July 20, 2025',
+        text: 'I am thrilled to announce the launch of atikle connect! <br> For too long, we have looked for a space that truly fosters connection and knowledge sharing without the noise. We decided to build it. <br> atikle connect is a meticulously created community platform designed for experts, creators, and curious minds to: <br>✨ Post valuable insights and spark meaningful conversations. <br>🎙️ Conduct engaging webinars to share expertise and build authority. <br> We believe in the power of connection, and we have built the home for it. This is community made right. <br> Ready to join a community that values your voice? <br> 🔗 [Link to your website/platform] <br> #AtikleConnect #NewLaunch #CommunityPlatform <br> </br> #ProfessionalDevelopment #KnowledgeSharing #Webinars #Networking #CommunityBuilding #TechLaunch',
         imageUrl: 'https://atikle.github.io/connect/community/anurag_gautam/posts/img-1.jpg'
-    },
-    {
-        authorUsername: 'anurag_gautam',
-        timestamp: 'September 2, 2025',
-        text: 'Just wrapped up a fantastic webinar on "The Future of Fintech." The engagement was incredible! The recording is now available in the webinars tab.',
-        imageUrl: null // Example of a post with no image
     }
-    // Add more post objects here
+    // {
+    //     authorUsername: 'anurag_gautam',
+    //     timestamp: 'September 2, 2025',
+    //     text: 'Just wrapped up a fantastic webinar on "The Future of Fintech." The engagement was incredible! The recording is now available in the webinars tab.',
+    //     imageUrl: null
+    // }
 ];
