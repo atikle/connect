0c4b795ae60b4e39915f47de2707fe6a23b77ee4
atikle <atikle.com@gmail.com>
2025-10-02 11:13:09 +0530

    Update

--- community/anurag_gautam/posts.js
+++ community/anurag_gautam/posts.js
@@ -1,6 +1,12 @@
 const posts = [
     {
         authorUsername: 'anurag_gautam',
+        timestamp: 'October 1, 2025',
+        text: 'Excited to share the new atikle account login page!. What do you think?',
+        imageUrl: 'https://atikle.github.io/connect/community/anurag_gautam/posts/img-1.jpg'
+    },
+    {
+        authorUsername: 'anurag_gautam',
         timestamp: 'September 4, 2025',
         text: 'Excited to share the revamped profile page for Atikle Connect! Added smooth animations and new sections for posts and webinars. What do you think?',
         imageUrl: 'https://images.unsplash.com/photo-1555066931-4365d14bab8c?q=80&w=2070'
